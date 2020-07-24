905c294978a2ac04593b59f6859a73b48e302dda
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-07-24 19:13:13 +0530

    Update Prime.py

--- Prime_number/Prime.py
+++ Prime_number/Prime.py
@@ -1,1 +1,10 @@
+#First python program to check online compiler & indentation
+# Program - Prime Number
+#I/p - Integer number  
+#o/p - Prime or not
 
+num = int(input("Enter a number "))
+if num%2==0:
+    print("Not a prime number")
+else:
+    print("Prime number")
