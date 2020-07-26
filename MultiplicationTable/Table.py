d999501bd132bc1c18bb334103511aefe868524d
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-07-26 19:28:22 +0530

    Update Table.py

--- MultiplicationTable/Table.py
+++ MultiplicationTable/Table.py
@@ -1,1 +1,7 @@
+#Multiplication table 
 
+num = int(input("Enter a number: "))
+for i in range(1,11):
+    ans = num*i
+    str = "{} x {} = {}" #because we cannot concatenate str and int 
+    print(str.format(num,i,ans))
