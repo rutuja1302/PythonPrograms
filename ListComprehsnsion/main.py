06e3451c8542dbdf190e37983b498cf8d88dedbc
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-08-04 19:01:46 +0530

    Update main.py

--- ListComprehsnsion/main.py
+++ ListComprehsnsion/main.py
@@ -1,1 +1,8 @@
+#List Comprehension problem
 
+x = int(input("x: "))
+y = int(input("y: "))
+z = int(input("z: "))
+n = int(input("n: "))
+
+print([[i,j,k] for i in range (x+1) for j in range (y+1) for k in range (z+1) if(i+j+k != n)])
