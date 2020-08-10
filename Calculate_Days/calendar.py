8d36189c419cbd969b7fda1ab79f4eebd8745ec9
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-08-10 19:58:08 +0530

    Update calendar.py

--- Calculate_Days/calendar.py
+++ Calculate_Days/calendar.py
@@ -1,1 +1,13 @@
-
+import calendar
+print("Team meeting will be on: ")
+for m in range(1,13):
+  cal = calendar.monthcalendar(2020,m)
+  weekone = cal[0]
+  weektwo = cal[1]
+  
+  if weekone[calendar.FRIDAY] != 0:
+    meetday = weekone[calendar.FRIDAY]
+  else:
+    meetday = weektwo[calendar.FRIDAY]
+    
+  print("%10s %2d" %(calendar.month_name[m],meetday))
