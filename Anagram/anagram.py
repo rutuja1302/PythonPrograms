70c55492b32960db74c4eb0a990699f962827e5b
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-08-12 18:55:43 +0530

    Update anagram.py

--- Anagram/anagram.py
+++ Anagram/anagram.py
@@ -1,1 +1,29 @@
+#Anagram Detection 
 
+#reads 2 strings
+st1 = input("Enter a string: ")
+st2 = input("Enter another string: ")
+
+#check if their lengths are equal
+if len(st1) == len(st2):
+  #create a list
+  n = len(st1)-1
+  list1 = list()
+  while n >= 0:
+    list1.append(st1[n])
+    n -= 1
+  m = len(st2)-1
+  list2 = list()
+  while m >= 0:
+    list2.append(st2[m])
+    m -= 1
+  #sort the list
+  list1.sort()
+  list2.sort()
+  #check if lists are equal and print the result
+  if list1 == list2:
+    print("Anagram")
+  else:
+    print("Not an anagram")
+else:
+  print("Not an anagram")
