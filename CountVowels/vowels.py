1e5c54de830f77284662ffa4faa530e167603158
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-08-16 19:33:37 +0530

    Update vowels.py

--- CountVowels/vowels.py
+++ CountVowels/vowels.py
@@ -1,1 +1,17 @@
+#count vowels in a string 
+#good news for you is the python already has a function call count() in strings
+#so we are just going to implement that
 
+#read a string
+st = input("Enter a string: ")
+
+#lower case all the characters as the function is case sensitive
+st = st.lower()
+
+#count all the vowels and add them
+countVowels = st.count('a') + st.count('e') + st.count('i') + st.count('o') + st.count('u')
+
+#print the result
+print()
+print("Number of vowels in the string: ")
+print(countVowels)
