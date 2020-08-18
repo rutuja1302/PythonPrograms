cd31778c7c192586e0a452bce44b2e52bd38e6db
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-08-18 19:41:02 +0530

    Update age.py

--- AgeCalculator/age.py
+++ AgeCalculator/age.py
@@ -1,1 +1,15 @@
+#age calulator
 
+from datetime import date
+from datetime import datetime
+
+print("Your date of birth (dd mm yyyy)")
+date_of_birth = datetime.strptime(input("--->"), "%d %m %Y")
+
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+age = calculate_age(date_of_birth)
+
+print("Your age is: ",age)
