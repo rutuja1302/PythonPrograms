f2414889f3af5cdb6394194f069f1db40ef0daa8
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-08-28 19:00:41 +0530

    Update main.py

--- SwapCases/main.py
+++ SwapCases/main.py
@@ -1,1 +1,11 @@
+#Swap case program will change upper case letter to lower case & vice versa
+# python has a inbuilt string method to swap cases called swapcase()
 
+def swap_case(s):
+    res = s.swapcase()
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
