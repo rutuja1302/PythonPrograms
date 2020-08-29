8c502e9848c674edf18836ec5fc95788568b3d63
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-08-29 19:49:53 +0530

    Update main.py

--- Mutations/main.py
+++ Mutations/main.py
@@ -1,1 +1,18 @@
+#Sample input: abracadabra
+#              5 k
+#SAmple output:abrackdabra
 
+def mutate_string(string, position, character):
+    mylist = list(string)
+    mylist[position] = character
+    
+    newStr = ""
+    for i in mylist:
+        newStr = newStr+i
+    return  newStr
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
