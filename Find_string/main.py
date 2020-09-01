5501b0552558b62b2045f1efbfbba06f0901dcbc
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-09-01 18:38:32 +0530

    Update main.py

--- Find_string/main.py
+++ Find_string/main.py
@@ -1,1 +1,15 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(0,len(string)):
+        start = i
+        end = i+len(sub_string)
+        if string[start:end] == sub_string:
+            count +=1
+    return count
 
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
