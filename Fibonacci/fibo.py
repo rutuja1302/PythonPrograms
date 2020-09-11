d5c15cd6bf7f74493b13a388587f3211bcbf6db5
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-09-11 21:03:43 +0530

    Update fibo.py

--- Fibonacci/fibo.py
+++ Fibonacci/fibo.py
@@ -1,1 +1,16 @@
+#fabonacci sequence for n numbers
 
+#read n
+n = int(input("Enter n: "))
+
+#intialize first, second term & sum
+ft = 0
+st = 1
+sum = 0
+
+#print remaining sequence
+for i in range(0,n):
+  sum = ft + sum
+  print(sum,end=" ")
+  ft = st
+  st = sum
