b992d80e4d4427b604a9d3cf6c1c04d4da7acb4c
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-09-13 19:28:42 +0530

    Update harmonic.py

--- Harmonic Number/harmonic.py
+++ Harmonic Number/harmonic.py
@@ -1,1 +1,21 @@
+#Harmonic Number
+#Logic - H1 = 1
+#        H2 = H1 + 1/2
+#        H3 = H2 + 1/3         
+#        Hn = Hn-1 + 1/n
 
+#read input n
+n = int(input("Enter a number: "))
+
+#initializing hn(harmonic number)
+hn = 1
+
+#ensuring n!=0
+if n!=0:
+    for i in range(2,n+1):
+        hn = hn + 1/i
+
+    print("Harmonic value for "+str(n)+" is:")
+    print(hn)
+else:
+    raise Exception("Invalid Number")
