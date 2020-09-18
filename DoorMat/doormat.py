f5ef79d2c42ec77985516f513bad8550c3ae9a1c
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-09-18 19:10:04 +0530

    Added program code

--- DoorMat/doormat.py
+++ DoorMat/doormat.py
@@ -1,1 +1,21 @@
+n,m = map(int, input().split())
+width = m
+msg = "WELCOME"
+design = ".|."
+#upper piece
+lines = int((n-1)/2)
+count = 1
+for i in range(1,lines+1):
+    a = design*count
+    print(a.center(width,'-'))
+    count += 2
 
+#center piece
+print(msg.center(width,'-'))
+
+#bottom piece
+count = n-2
+for i in range(1,lines+1):
+    a = design*count
+    print(a.center(width,'-'))
+    count -= 2
